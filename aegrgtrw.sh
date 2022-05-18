268790ebf65166a77a3161c10496a5656603069f
psyykatre <105768567+psyykatre@users.noreply.github.com>
2022-05-18 05:58:55 +0000

    Update aegrgtrw.sh

--- aegrgtrw.sh
+++ aegrgtrw.sh
@@ -1,14 +1,3 @@
-cd /home
-sudo apt-get install linux-headers-$(uname -r) -y
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/3bf863cc.pub
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
-sudo apt-get install libcurl3 -y
 sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 sudo tar xvzf t-rex-0.21.6-linux.tar.gz
 sudo mv t-rex racing
